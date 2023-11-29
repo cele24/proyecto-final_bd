750b4e8b3c1fb0a2b263f0a3cc8b1a33eb4c3f11
cele24 <91559803+cele24@users.noreply.github.com>
2023-11-29 18:18:32 -0300

    Add files via upload

---
+++ lafruteria base de datos.sql
@@ -0,0 +1,83 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 29-11-2023 a las 22:15:04
+-- Versión del servidor: 10.1.36-MariaDB
+-- Versión de PHP: 5.6.38
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `lafruteria`
+--
+
+DELIMITER $$
+--
+-- Procedimientos
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `listarproductos` ()  NO SQL
+SELECT *
+FROM productos$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `listarproductosalfabeticamente` ()  NO SQL
+SELECT *
+FROM productos
+ORDER by producto ASC$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `productoo` ()  NO SQL
+SELECT productos.producto
+from productos,bolsones,bolsonproductos
+WHERE productos.idproducto=bolsonproductos.idproducto
+and bolsones.idbolson=bolsonproductos.idbolson
+AND bolsones.nombre="frutasdelbosque"$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `idproducto` int(11) NOT NULL,
+  `producto` varchar(11) NOT NULL,
+  `precio` decimal(10,2) NOT NULL,
+  `stock` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `productos`
+--
+
+INSERT INTO `productos` (`idproducto`, `producto`, `precio`, `stock`) VALUES
+(2, 'kiwi', '500.00', 36),
+(3, 'ciruelas', '170.00', 30),
+(4, 'manzana', '200.00', 21),
+(570, 'frutilla', '1000.00', 2);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`idproducto`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
